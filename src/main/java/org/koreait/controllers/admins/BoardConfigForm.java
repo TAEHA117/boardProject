6d3c63aaf21390a4299180b94938e3aa8ab322b8
TAEHA117 <>
2023-11-23 17:07:19 +0900

    중간 업로드 완료

--- src/main/java/org/koreait/controllers/admins/BoardConfigForm.java
+++ src/main/java/org/koreait/controllers/admins/BoardConfigForm.java
@@ -10,8 +10,11 @@ import lombok.NoArgsConstructor;
 @Data @Builder
 @NoArgsConstructor @AllArgsConstructor
 public class BoardConfigForm {
+
+    private String mode;
+
     @NotBlank(message="게시판 아이디를 입력하세요") // 필수항목
-    private String bid; // 게시판 아이디 - 필수항목
+    private String bId; // 게시판 아이디 - 필수항목
 
     @NotBlank(message="게시판 이름을 입력하세요") // 필수항목
     private String bName; // 게시판 이름 - 필수항목
